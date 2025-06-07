329a86176a064551b0605dddbb345b9581786512
zarif93 <danielzarif93@gmail.com>
2025-06-07 05:08:13 +0300

    fix

--- clubgg_session.py
+++ clubgg_session.py
@@ -37,14 +37,14 @@ def login_to_clubgg():
     password = os.getenv("PASSWORD")
 
     with sync_playwright() as p:
-        browser = p.firefox.launch(headless=True)  # או השמטת headless, זה ברירת מחדל
+        browser = p.firefox.launch(headless=True, slow_mo=500)  # או השמטת headless, זה ברירת מחדל
         context = browser.new_context(
             user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
             viewport={"width": 1280, "height": 720}
         )
         page = context.new_page()
         print("Starting navigation to login page...")
-        page.goto("https://union.clubgg.com/login", wait_until="domcontentloaded", timeout=60000)
+        page.goto("https://union.clubgg.com/login", wait_until="networkidle", timeout=60000)
         print("Page loaded")
         # ...
         print("Navigation complete")
